bc272f3a19a5202688ef48ab6d065965a36af60b
imdhemy <imdhemy@gmail.com>
2024-12-09 20:24:47 +0100

    chore: add getter tests

--- tests/Unit/Http/Requests/RegistrationRequestTest.php
+++ tests/Unit/Http/Requests/RegistrationRequestTest.php
@@ -3,10 +3,18 @@
 namespace Tests\Unit\Http\Requests;
 
 use App\Http\Requests\RegistrationRequest;
+use Faker\Factory;
 use PHPUnit\Framework\TestCase;
 
 class RegistrationRequestTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->faker = Factory::create();
+    }
+
     public function test_it_is_a_public_request(): void
     {
         $request = new RegistrationRequest();
@@ -31,9 +39,24 @@ class RegistrationRequestTest extends TestCase
 
     public function test_get_email(): void
     {
+        $faker = Factory::create();
+        $email = $faker->email();
+        $sut = new RegistrationRequest(compact('email'));
+
+        $actual = $sut->getEmail();
+
+        $this->assertSame($email, $actual);
     }
 
     public function test_get_password(): void
     {
+        $faker = Factory::create();
+        $password = $faker->password();
+        $sut = new RegistrationRequest();
+        $sut->merge(['password' => $password]);
+
+        $actual = $sut->getPassword();
+
+        $this->assertSame($password, $actual);
     }
 }
