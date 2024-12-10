a11e1dd5dfea24e73d07ce2e018c1c7b1be33625
Shimaa Essam <125494212+shimaa123456@users.noreply.github.com>
2024-12-10 10:42:44 +0200

    feat: add Post mode

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'body',
+        'user_id',
+    ];
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
