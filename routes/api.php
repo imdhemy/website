ed155c6858b2d7e7dc23a2a7898efa9f3733339d
ABDELAZIZEZZT <zezo.nassar444@gmail.com>
2024-12-10 02:34:12 +0200

    add:login route

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/v1/auth/registration', [AuthController::class, 'register']);
+Route::post('/v1/auth/login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
