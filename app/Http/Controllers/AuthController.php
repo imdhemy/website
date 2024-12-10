ea32492f3c2eb78fd4aa4a2f331349266e8d79af
ABDELAZIZEZZT <zezo.nassar444@gmail.com>
2024-12-10 02:32:42 +0200

    feat:login function in Auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegistrationRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
@@ -18,4 +20,24 @@ class AuthController extends Controller
 
         return response()->json($user, Response::HTTP_CREATED);
     }
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $email=$request->email();
+        $password=$request->password();
+
+
+        $check=Auth::attempt(['email'=>$email,'password'=>$password]);
+        if(!$check){
+            return response()->json(['message'=>'the password not correct'],);
+        }
+
+        $user=User::where('email',$email)->first();
+        $token = $user->createToken($user->name.'-AuthToken')->plainTextToken;
+
+        return response()->json([
+            'user'=>$user,
+            'token'=>$token,
+        ],200);
+    }
+
 }
