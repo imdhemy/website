4b5a2b00b5ffb2161baaa03dbf6e8557cb04a5d9
Sara Amin <sarah.ammen14@gmail.com>
2024-12-14 20:24:32 +0200

    make test for function index for show all data

--- tests/Feature/Post/ViewPostsTest.php
+++ tests/Feature/Post/ViewPostsTest.php
@@ -2,19 +2,24 @@
 
 namespace Tests\Feature\Post;
 
+use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class ViewPostsTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    public function test_index_all_posts(): void
     {
-        $response = $this->get('/');
+        $posts = Post::factory()->create();
+        $posts= $posts->all();
 
-        $response->assertStatus(200);
+
+        $response = $this->getJson('api/v1/index/posts' ,$posts->toArray());
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonStructure(['id','title','body','user_id']);
+        $this->assertDatabaseHas('posts',$posts->toArray());
     }
 }
